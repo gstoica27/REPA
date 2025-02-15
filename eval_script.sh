a35e07c860d47e131aef1aa60c81da434c37f6ab
gstoica27 <root@triton-cs-aus-455.reviz.ai2.in>
2025-02-14 17:18:50 -0800

    run on gantry

--- eval_script.sh
+++ eval_script.sh
@@ -2,7 +2,7 @@
 MODEL_NAME="SiT-B/2"
 # Compute this list using the utils find_experiment_paths -> convert_pylist_to_shlist functions! 
 # for exp_name in "structImg-0p5-repaLinear-0p5-sitb2-dinov2VitB-enc4-bs256" "structImg-noRelu-0p5-repaLinear-0p5-sitb2-dinov2VitB-enc4-bs256" "structTok-0p5-repaLinear-0p5-sitb2-dinov2VitB-enc4-bs256" "structTok-noRelu-0p5-repaLinear-0p5-sitb2-dinov2VitB-enc4-bs256" "structImgByTok-0p5-repaLinear-0p5-sitb2-dinov2VitB-enc4-bs256" "structImgByTok-noRelu-0p5-repaLinear-0p5-sitb2-dinov2VitB-enc4-bs256" 
-for exp_name in "repaLinear-0p5-sitb2-dinov2VitB-enc4-bs256-tripmseTemp0p1" "repaLinear-0p5-sitb2-dinov2VitB-enc4-bs512-tripmseTemp0p05" "linear-sitb-dinov2-b-enc4/between_images-structCoeff_0.0" "repaLinear-0p5-sitb2-dinov2VitB-enc4-bs512"
+for exp_name in "repaLinear-0p5-sitb2-dinov2VitB-enc4-bs256-tripmseTemp0p1" #"repaLinear-0p5-sitb2-dinov2VitB-enc4-bs512-tripmseTemp0p05" "linear-sitb-dinov2-b-enc4/between_images-structCoeff_0.0" "repaLinear-0p5-sitb2-dinov2VitB-enc4-bs512"
 do
     # EXP_LOC="exps/${exp_name}"
     EXP_LOC="/weka/prior-default/georges/research/REPA/exps/${exp_name}"
@@ -20,7 +20,7 @@ do
                 --num-fid-samples 50000 \
                 --ckpt "${EXP_LOC}/checkpoints/${fname}" \
                 --path-type=linear \
-                --encoder-depth=4 \
+                --encoder-depth=8 \
                 --projector-embed-dims=768 \
                 --per-proc-batch-size=64 \
                 --mode=sde \
