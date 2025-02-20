c06f5fe138e37ae7a25c82d7572750fbc91f778a
gstoica27 <gstoica27@gmail.com>
2025-02-19 22:07:38 -0800

    Debugging

--- eval_script.sh
+++ eval_script.sh
@@ -7,8 +7,10 @@ CHECKPOINT_ITERS="0050000.pt 0100000.pt 0150000.pt 0200000.pt 0250000.pt 0300000
 for exp_name in $MODEL_ITERS
 do 
     for fname in $CHECKPOINT_ITERS
-    do 
-        echo $exp_name $fname
+    do
+        if [ ! -d "${SAVE_DIR}/${exp_name}" ]; then   
+            echo $exp_name $fname
+        fi
     done 
 done
 
@@ -17,29 +19,29 @@ do
     # EXP_LOC="exps/${exp_name}"
     EXP_LOC="/weka/prior-default/georges/research/REPA/exps/${exp_name}"
     SAVE_DIR="/weka/prior-default/georges/research/REPA/samples/fid_50k/50_steps/${exp_name}"
-    if [ ! -d "${SAVE_DIR}" ]; then
         # for fname in "0050000.pt" "0100000.pt" "0150000.pt" "0200000.pt" "0250000.pt" "0300000.pt" "0350000.pt" "0400000.pt"
-        for fname in $CHECKPOINT_ITERS
-            do 
-                torchrun \
-                --nnodes=1 \
-                --nproc_per_node=8 \
-                --master-port 29501 \
-                generate.py \
-                --model "${MODEL_NAME}" \
-                --num-fid-samples 50000 \
-                --ckpt "${EXP_LOC}/checkpoints/${fname}" \
-                --path-type=linear \
-                --encoder-depth=8 \
-                --projector-embed-dims=768 \
-                --per-proc-batch-size=64 \
-                --mode=sde \
-                --num-steps=50 \
-                --cfg-scale=1.0 \
-                --guidance-high=0.0 \
-                --sample-dir "${SAVE_DIR}"
-            done 
-    fi
+    for fname in $CHECKPOINT_ITERS
+        do
+        if [ ! -d "${SAVE_DIR}/${exp_name}" ]; then 
+            torchrun \
+            --nnodes=1 \
+            --nproc_per_node=8 \
+            --master-port 29501 \
+            generate.py \
+            --model "${MODEL_NAME}" \
+            --num-fid-samples 50000 \
+            --ckpt "${EXP_LOC}/checkpoints/${fname}" \
+            --path-type=linear \
+            --encoder-depth=8 \
+            --projector-embed-dims=768 \
+            --per-proc-batch-size=64 \
+            --mode=sde \
+            --num-steps=50 \
+            --cfg-scale=1.0 \
+            --guidance-high=0.0 \
+            --sample-dir "${SAVE_DIR}"
+        fi
+    done 
 done
 
 for exp_name in $MODEL_ITERS
@@ -47,27 +49,28 @@ do
     # EXP_LOC="exps/${exp_name}"
     EXP_LOC="/weka/prior-default/georges/research/REPA/exps/${exp_name}"
     SAVE_DIR="/weka/prior-default/georges/research/REPA/samples/fid_50k/250_steps/${exp_name}"
-    if [ ! -d "${SAVE_DIR}" ]; then
-        for fname in $CHECKPOINT_ITERS
-        # for fname in "0400000.pt"
-            do 
-                torchrun \
-                --nnodes=1 \
-                --nproc_per_node=8 \
-                --master-port 29502 \
-                generate.py \
-                --model "${MODEL_NAME}" \
-                --num-fid-samples 50000 \
-                --ckpt "${EXP_LOC}/checkpoints/${fname}" \
-                --path-type=linear \
-                --encoder-depth=8 \
-                --projector-embed-dims=768 \
-                --per-proc-batch-size=64 \
-                --mode=sde \
-                --num-steps=250 \
-                --cfg-scale=1.0 \
-                --guidance-high=0.0 \
-                --sample-dir "${SAVE_DIR}"
-            done 
-    fi
+    # if [ ! -d "${SAVE_DIR}" ]; then
+    for fname in $CHECKPOINT_ITERS
+    # for fname in "0400000.pt"
+        do
+        if [ ! -d "${SAVE_DIR}/${exp_name}" ]; then  
+            torchrun \
+            --nnodes=1 \
+            --nproc_per_node=8 \
+            --master-port 29502 \
+            generate.py \
+            --model "${MODEL_NAME}" \
+            --num-fid-samples 50000 \
+            --ckpt "${EXP_LOC}/checkpoints/${fname}" \
+            --path-type=linear \
+            --encoder-depth=8 \
+            --projector-embed-dims=768 \
+            --per-proc-batch-size=64 \
+            --mode=sde \
+            --num-steps=250 \
+            --cfg-scale=1.0 \
+            --guidance-high=0.0 \
+            --sample-dir "${SAVE_DIR}"
+        fi
+    done 
 done
