0a1583f7b437382e6ea3d79e511e5b2c573f2885
gstoica27 <gstoica27@gmail.com>
2025-02-27 23:52:42 -0800

    changed to generate with intermediates

--- evalute_denoising_steps.sh
+++ evalute_denoising_steps.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 MODEL_NAME="SiT-XL/2"
-MODEL_ITERS="linear-dinov2-b-enc8"
-# MODEL_ITERS="repaLinear-0p5-sitxl2-dinov2VitB-enc8-bs256-tripanyTemp0p05"
+# MODEL_ITERS="linear-dinov2-b-enc8"
+MODEL_ITERS="repaLinear-0p5-sitxl2-dinov2VitB-enc8-bs256-tripanyTemp0p05"
 CHECKPOINT_FNAME="0400000.pt"
-STEPS="50"
+STEPS="250"
 
 
 for steps in $STEPS
