fa8bc166de51c2d90057af4da45dd0263bdaccbc
gstoica27 <gstoica27@gmail.com>
2025-02-28 21:44:28 -0800

    deleted skip

--- evaluate_trajectories.sh
+++ evaluate_trajectories.sh
@@ -14,27 +14,27 @@ do
         SAVE_DIR="/weka/prior-default/georges/research/REPA/samples_analysis/${steps}_steps/${exp_name}"
         for trajectory_structure_type in $TRAJECTORY_STRUCTURE_TYPES 
         do
-            if [ ! -d "${SAVE_DIR}/${exp_name}" ]; then 
-                torchrun \
-                --nnodes=1 \
-                --nproc_per_node=8 \
-                --master-port 29501 \
-                generate_with_intermediates.py \
-                --model "${MODEL_NAME}" \
-                --num-fid-samples 10000 \
-                --ckpt "${EXP_LOC}/checkpoints/${CHECKPOINT_FNAME}" \
-                --path-type=linear \
-                --encoder-depth=8 \
-                --projector-embed-dims=768 \
-                --per-proc-batch-size=64 \
-                --mode=sde \
-                --num-steps=$steps \
-                --cfg-scale=1.0 \
-                --guidance-high=0.0 \
-                --sample-dir "${SAVE_DIR}" \
-                --record-trajectory-structure \
-                --trajectory-structure-type $trajectory_structure_type
-            fi
+            # if [ ! -d "${SAVE_DIR}/${exp_name}" ]; then 
+            torchrun \
+            --nnodes=1 \
+            --nproc_per_node=8 \
+            --master-port 29501 \
+            generate_with_intermediates.py \
+            --model "${MODEL_NAME}" \
+            --num-fid-samples 10000 \
+            --ckpt "${EXP_LOC}/checkpoints/${CHECKPOINT_FNAME}" \
+            --path-type=linear \
+            --encoder-depth=8 \
+            --projector-embed-dims=768 \
+            --per-proc-batch-size=64 \
+            --mode=sde \
+            --num-steps=$steps \
+            --cfg-scale=1.0 \
+            --guidance-high=0.0 \
+            --sample-dir "${SAVE_DIR}" \
+            --record-trajectory-structure \
+            --trajectory-structure-type $trajectory_structure_type
+            # fi
         done
     done 
 done
