f18210351b59b13209667a79c9e933e06df7651d
Xiang Fan <5893440+sfanxiang@users.noreply.github.com>
2025-03-01 02:00:45 +0000

    Update eval_script_with_cfg.sh

--- eval_script_with_cfg.sh
+++ eval_script_with_cfg.sh
@@ -3,7 +3,7 @@
 MODEL_NAME="SiT-XL/2"
 # MODEL_ITERS="repaLinear-0p5-sitb2-dinov2VitB-enc4-bs512 repaLinear-0p5-sitb2-dinov2VitB-enc4-bs512-tripmseTemp0p05 repaLinear-0p5-sitb2-dinov2VitB-enc4-bs1024 repaLinear-0p5-sitb2-dinov2VitB-enc4-bs1024-tripanyTemp0p1"
 MODEL_ITERS="linear-dinov2-b-enc8 repaLinear-0p5-sitxl2-dinov2VitB-enc8-bs256-tripanyTemp0p05"
-CHECKPOINT_ITERS="0050000.pt 0100000.pt 0150000.pt 0200000.pt 0250000.pt 0300000.pt 0350000.pt 0400000.pt"
+CHECKPOINT_ITERS="0400000.pt"
 # Compute this list using the utils find_experiment_paths -> convert_pylist_to_shlist functions! 
 for exp_name in $MODEL_ITERS
 do 
@@ -18,8 +18,8 @@ done
 for exp_name in $MODEL_ITERS
 do
     # EXP_LOC="exps/${exp_name}"
-    EXP_LOC="/weka/prior-default/georges/research/REPA/exps2/${exp_name}"
-    SAVE_DIR="/weka/prior-default/georges/research/REPA/samples/fid_50k/50_steps/with_cfg/${exp_name}"
+    EXP_LOC="/data/xiangf/REPA/exps2/${exp_name}"
+    SAVE_DIR="/data/xiangf/REPA/samples/fid_50k/50_steps/with_cfg/${exp_name}"
     for fname in $CHECKPOINT_ITERS
         do
         if [ ! -d "${SAVE_DIR}/${exp_name}" ]; then 
@@ -36,9 +36,9 @@ do
             --projector-embed-dims=768 \
             --per-proc-batch-size=64 \
             --mode=sde \
-            --num-steps=50 \
-            --cfg-scale=1.8 \
-            --guidance-high=0.7 \
+            --num-steps=$3 \
+            --cfg-scale=$1 \
+            --guidance-high=$2 \
             --sample-dir "${SAVE_DIR}"
         fi
     done 
