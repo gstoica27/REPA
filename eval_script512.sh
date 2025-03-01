c736f1bfb1703f4e763dfa6f169e0c49379b19ac
gstoica27 <gstoica27@gmail.com>
2025-02-28 19:26:01 -0800

    SiT-XL/2 Experiments

--- eval_script512.sh
+++ eval_script512.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-MODEL_NAME="SiT-B/2"
-# MODEL_NAME="SiT-XL/2"
+# MODEL_NAME="SiT-B/2"
+MODEL_NAME="SiT-XL/2"
 # MODEL_ITERS="repaLinear-0p5-sitxl2-dinov2VitB-enc8-bs256-tripanyTemp0p001 repaLinear-0p5-sitxl2-dinov2VitB-enc8-bs256-tripanyTemp0p01 repaLinear-0p5-sitxl2-dinov2VitB-enc8-bs256-tripanyTemp0p15"
-MODEL_ITERS="repaLinear-0p5-sitb2-dinov2VitB-enc4-bs256-meanTemp0p0-res512 repaLinear-0p5-sitb2-dinov2VitB-enc4-bs256-tripanyTemp0p05-res512 repaLinear-0p5-sitb2-dinov2VitB-enc4-bs256-tripsameTemp0p05-res512"
+# MODEL_ITERS="repaLinear-0p5-sitb2-dinov2VitB-enc4-bs256-meanTemp0p0-res512 repaLinear-0p5-sitb2-dinov2VitB-enc4-bs256-tripanyTemp0p05-res512 repaLinear-0p5-sitb2-dinov2VitB-enc4-bs256-tripsameTemp0p05-res512"
+MODEL_ITERS="repaLinear-0p5-sitxl2-dinov2VitB-enc8-bs256-meanTemp0p0-res512 repaLinear-0p5-sitxl2-dinov2VitB-enc8-bs256-tripanyTemp0p05-res512"
 # CHECKPOINT_ITERS="0050000.pt 0100000.pt 0150000.pt 0200000.pt 0250000.pt 0300000.pt 0350000.pt 0400000.pt"
 CHECKPOINT_ITERS="0400000.pt"
 # Compute this list using the utils find_experiment_paths -> convert_pylist_to_shlist functions! 
@@ -20,7 +21,7 @@ for exp_name in $MODEL_ITERS
 do
     # EXP_LOC="exps/${exp_name}"
     EXP_LOC="/weka/prior-default/georges/research/REPA/exps2/${exp_name}"
-    SAVE_DIR="/weka/prior-default/georges/research/REPA/samples/fid_50k/50_steps/${exp_name}"
+    SAVE_DIR="/weka/prior-default/georges/research/REPA/samples_res512/fid_50k/50_steps/${exp_name}"
         # for fname in "0050000.pt" "0100000.pt" "0150000.pt" "0200000.pt" "0250000.pt" "0300000.pt" "0350000.pt" "0400000.pt"
     for fname in $CHECKPOINT_ITERS
         do
@@ -51,7 +52,7 @@ for exp_name in $MODEL_ITERS
 do
     # EXP_LOC="exps/${exp_name}"
     EXP_LOC="/weka/prior-default/georges/research/REPA/exps2/${exp_name}"
-    SAVE_DIR="/weka/prior-default/georges/research/REPA/samples/fid_50k/250_steps/${exp_name}"
+    SAVE_DIR="/weka/prior-default/georges/research/REPA/samples_res512/fid_50k/250_steps/${exp_name}"
     # if [ ! -d "${SAVE_DIR}" ]; then
     for fname in $CHECKPOINT_ITERS
     # for fname in "0400000.pt"
