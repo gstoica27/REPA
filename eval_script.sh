219b7a08edbbd30ddc464354d344f6450df68c2e
gstoica27 <gstoica27@gmail.com>
2025-03-02 14:22:22 -0800

    refactored eval_script, running sitb

--- eval_script.sh
+++ eval_script.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 MODEL_NAME="SiT-B/2"
 # MODEL_NAME="SiT-XL/2"
-MODEL_ITERS="linear-sitb-dinov2-b-enc4/between_images-structCoeff_0.5 repaLinear-0p5-sitb2-dinov2VitB-enc4-bs256-tripmseTemp0p5"
+MODEL_ITERS="repaLinear-0p5-sitb2-dinov2VitB-enc4-bs256-tripanyTemp0p05-res256 sitb2-vanilla-bs256-tripanyTemp0p05-res256"
 # MODEL_ITERS="sitxl2-vanilla-bs256-meanTemp0p0-res256 sitxl2-vanilla-bs256-tripanyTemp0p05-res256"
 CHECKPOINT_ITERS="0050000.pt 0100000.pt 0150000.pt 0200000.pt 0250000.pt 0300000.pt 0350000.pt 0400000.pt"
 # MODEL_ITERS="repaLinear-0p5-sitxl2-dinov2VitB-enc8-bs512-meanTemp0p0-res256 repaLinear-0p5-sitxl2-dinov2VitB-enc8-bs512-tripanyTemp0p05-res256"
 # CHECKPOINT_ITERS="0400000.pt"
 # Compute this list using the utils find_experiment_paths -> convert_pylist_to_shlist functions! 
+# SAVE_DIR="/weka/prior-default/georges/research/REPA/samples/fid_50k"
+SAVE_DIR="/weka/oe_training_default/georges/research/REPA/samples/fid_50k"
+
 for exp_name in $MODEL_ITERS
 do 
     for fname in $CHECKPOINT_ITERS
@@ -19,10 +22,8 @@ done
 
 for exp_name in $MODEL_ITERS
 do
-    # EXP_LOC="exps/${exp_name}"
     EXP_LOC="/weka/prior-default/georges/research/REPA/exps2/${exp_name}"
-    SAVE_DIR="/weka/prior-default/georges/research/REPA/samples/fid_50k/50_steps/${exp_name}"
-        # for fname in "0050000.pt" "0100000.pt" "0150000.pt" "0200000.pt" "0250000.pt" "0300000.pt" "0350000.pt" "0400000.pt"
+    EXP_SAVE_DIR="${SAVE_DIR}/50_steps/${exp_name}"
     for fname in $CHECKPOINT_ITERS
         do
         if [ ! -d "${SAVE_DIR}/${exp_name}" ]; then 
@@ -42,7 +43,7 @@ do
             --num-steps=50 \
             --cfg-scale=1.0 \
             --guidance-high=0.0 \
-            --sample-dir "${SAVE_DIR}"
+            --sample-dir "${EXP_SAVE_DIR}"
         fi
     done 
 done
@@ -51,10 +52,8 @@ for exp_name in $MODEL_ITERS
 do
     # EXP_LOC="exps/${exp_name}"
     EXP_LOC="/weka/prior-default/georges/research/REPA/exps2/${exp_name}"
-    SAVE_DIR="/weka/prior-default/georges/research/REPA/samples/fid_50k/250_steps/${exp_name}"
-    # if [ ! -d "${SAVE_DIR}" ]; then
+    EXP_SAVE_DIR="${SAVE_DIR}/250_steps/${exp_name}"
     for fname in $CHECKPOINT_ITERS
-    # for fname in "0400000.pt"
         do
         if [ ! -d "${SAVE_DIR}/${exp_name}" ]; then  
             torchrun \
@@ -73,7 +72,7 @@ do
             --num-steps=250 \
             --cfg-scale=1.0 \
             --guidance-high=0.0 \
-            --sample-dir "${SAVE_DIR}"
+            --sample-dir "${EXP_SAVE_DIR}"
         fi
     done 
 done
