dbd1b4ac3667b47841f2f2ef75252e6e99aa64b0
gstoica27 <gstoica27@gmail.com>
2025-03-04 15:12:28 -0800

    denoising fewer steps

--- evaluate_denoising_steps.sh
+++ evaluate_denoising_steps.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 MODEL_NAME="SiT-XL/2"
-# MODEL_ITERS="repaLinear-0p5-sitxl2-dinov2VitB-enc8-bs256-tripanyTemp0p05 linear-dinov2-b-enc8"
-MODEL_ITERS="repaLinear-0p5-sitxl2-dinov2VitB-enc8-bs256-tripanyTemp0p05-res512 repaLinear-0p5-sitxl2-dinov2VitB-enc8-bs256-meanTemp0p0-res512"
+MODEL_ITERS="repaLinear-0p5-sitxl2-dinov2VitB-enc8-bs256-tripanyTemp0p05 linear-dinov2-b-enc8"
+# MODEL_ITERS="repaLinear-0p5-sitxl2-dinov2VitB-enc8-bs256-tripanyTemp0p05-res512 repaLinear-0p5-sitxl2-dinov2VitB-enc8-bs256-meanTemp0p0-res512"
 CHECKPOINT_FNAME="0400000.pt"
-STEPS="50"
+STEPS="30"
 
 
 for steps in $STEPS
@@ -11,7 +11,8 @@ do
     for exp_name in $MODEL_ITERS
         do
         EXP_LOC="/weka/prior-default/georges/research/REPA/exps2/${exp_name}"
-        SAVE_DIR="/weka/prior-default/georges/research/REPA/samples_analysis512/${steps}_steps/${exp_name}"
+        # SAVE_DIR="/weka/prior-default/georges/research/REPA/samples_analysis512/${steps}_steps/${exp_name}"
+        SAVE_DIR="/weka/prior-default/georges/research/REPA/samples_analysis/${steps}_steps/${exp_name}"
         if [ ! -d "${SAVE_DIR}/${exp_name}" ]; then 
             torchrun \
             --nnodes=1 \
@@ -31,10 +32,10 @@ do
             --guidance-high=0.0 \
             --sample-dir "${SAVE_DIR}" \
             --record-intermediate-steps \
-            --record-intermediate-steps-freq 10 \
+            --record-intermediate-steps-freq 5 \
             --record-custom-classes 153 88 2 417 933 555 932 385 386 294 33 207 250 387 402 620 812 429 388 360 291 89 928 973 302 340 511 609 878 919 888 698 866 576 547 13 14 18 17 31 96 145 148 327 403 873 839 820\
             --rough-examples-per-class 64 \
-            --resolution 512
+            --resolution 256
         fi
     done 
 done
