369016c52818a764d5c3924f316a2ee681e8f75a
gstoica27 <root@triton-cs-aus-454.reviz.ai2.in>
2025-04-06 13:51:55 -0700

    evaluating class conditioned

--- eval_script.sh
+++ eval_script.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 # MODEL_NAME="SiT-B/2"
 MODEL_NAME="SiT-XL/2"
-# MODEL_ITERS="linear-sitb-dinov2-b-enc4/between_images-structCoeff_0.0 repaLinear-0p5-sitb2-dinov2VitB-enc4-bs256-tripmseTemp0p5"
-# MODEL_ITERS="repaLinear-0p5-sitxl2-dinov2VitB-enc8-bs256-tripanyTemp0p05-res256/repaLinear-0p5-sitxl2-dinov2VitB-enc8-bs256-meanTemp0p0-res256"
-MODEL_ITERS="repaLinear-0p5-sitxl2-dinov2VitB-enc8-bs256-tripanyTemp0p046-res256 repaLinear-0p5-sitxl2-dinov2VitB-enc8-bs256-tripanyTemp0p047-res256 repaLinear-0p5-sitxl2-dinov2VitB-enc8-bs256-tripanyTemp0p048-res256 repaLinear-0p5-sitxl2-dinov2VitB-enc8-bs256-tripanyTemp0p049-res256 repaLinear-0p5-sitxl2-dinov2VitB-enc8-bs256-tripanyTemp0p05-res256"
-# MODEL_ITERS="repaLinear-0p5-sitxl2-dinov2VitB-enc8-bs256-tripanyTemp0p05-res256"
+# MODEL_ITERS="repaLinear-0p5-sitxl2-dinov2VitB-enc8-bs256-tripanyTemp0p046-res256 repaLinear-0p5-sitxl2-dinov2VitB-enc8-bs256-tripanyTemp0p047-res256 repaLinear-0p5-sitxl2-dinov2VitB-enc8-bs256-tripanyTemp0p048-res256 repaLinear-0p5-sitxl2-dinov2VitB-enc8-bs256-tripanyTemp0p049-res256 repaLinear-0p5-sitxl2-dinov2VitB-enc8-bs256-tripanyTemp0p05-res256"
+MODEL_ITERS="repaLinear-0p5-sitxl2-dinov2VitB-enc8-bs256-cctripanyTemp0p01-res256  repaLinear-0p5-sitxl2-dinov2VitB-enc8-bs256-cctripanyTemp0p025-res256  repaLinear-0p5-sitxl2-dinov2VitB-enc8-bs256-cctripanyTemp0p05-res256  repaLinear-0p5-sitxl2-dinov2VitB-enc8-bs256-cctripanyTemp0p1-res256  repaLinear-0p5-sitxl2-dinov2VitB-enc8-bs256-cctripanyTemp0p2-res256"
 # CHECKPOINT_ITERS="0050000.pt 0100000.pt 0150000.pt 0200000.pt 0250000.pt 0300000.pt 0350000.pt 0400000.pt"
 # CHECKPOINT_ITERS="0050000.pt 0100000.pt 0150000.pt 0200000.pt 0250000.pt 0300000.pt 0350000.pt 0400000.pt 0450000.pt 0500000.pt 0550000.pt 0600000.pt 0650000.pt 0700000.pt 0750000.pt 0800000.pt 0850000.pt 0900000.pt 0950000.pt 1000000.pt 1050000.pt 1100000.pt 1150000.pt 1200000.pt 1250000.pt 1300000.pt 1350000.pt 1400000.pt 1450000.pt 1500000.pt 1550000.pt 1600000.pt 1650000.pt 1700000.pt 1750000.pt 1800000.pt"
 # CHECKPOINT_ITERS="1800000.pt 1750000.pt 1700000.pt 1650000.pt 1600000.pt 1550000.pt 1500000.pt 1450000.pt 1400000.pt 1350000.pt 1300000.pt 1250000.pt 1200000.pt 1150000.pt 1100000.pt 1050000.pt 1000000.pt 0950000.pt 0900000.pt 0850000.pt 0800000.pt 0750000.pt 0700000.pt 0650000.pt 0600000.pt 0550000.pt 0500000.pt 0450000.pt 0400000.pt 0350000.pt 0300000.pt 0250000.pt 0200000.pt 0150000.pt 0100000.pt 0050000.pt"
@@ -14,7 +12,8 @@ CHECKPOINT_ITERS="0400000.pt"
 SAVE_DIR="/weka/oe-training-default/georges/samples/masked_unconditionals/fid_50k"
 # SAVE_DIR="/weka/oe_training_default/georges/research/REPA/samples2/fid_50k/7M_models_reverse"
 # EXP_LOC="/weka/oe_training_default/georges/checkpoints/REPA/exps/exps2_7M"
-EXP_LOC="/weka/oe-training-default/georges/checkpoints/REPA/exps/masked_unconditionals/hyperparam_search"
+# EXP_LOC="/weka/oe-training-default/georges/checkpoints/REPA/exps/masked_unconditionals/hyperparam_search"
+EXP_LOC="/weka/oe-training-default/georges/checkpoints/REPA/exps/class_conditioned"
 
 for exp_name in $MODEL_ITERS
 do 
