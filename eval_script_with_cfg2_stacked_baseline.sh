d12c33eb17d39f25b1f7b7d1804e93947c22ced6
Xiang Fan <xiangfan00@gmail.com>
2025-05-15 20:16:57 -0700

    Pareto

---
+++ eval_script_with_cfg2_stacked_baseline.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 1.25 - 2.25
+# 0.5 - 1.0
+# 75 - 150
+
+args="--beaker-image ai2/cuda11.8-cudnn8-dev-ubuntu20.04 --weka prior-default:/weka/prior-default --weka oe-training-default:/weka/oe-training-default --budget ai2/prior --shared-memory 800G --priority high --allow-dirty --retries 3"
+
+gantry run $args --workspace ai2/structured_diffusion --cluster ai2/jupiter-cirrascale-2 --cluster ai2/saturn-cirrascale --gpus 8 --name SiTXL2-StackedCFG-Baseline-cfg-1.85-0.65-50 -- bash eval_script_with_cfg_stacked_baseline.sh 1.85 0.65 50
+gantry run $args --workspace ai2/structured_diffusion --cluster ai2/jupiter-cirrascale-2 --cluster ai2/saturn-cirrascale --gpus 8 --name SiTXL2-StackedCFG-Baseline-cfg-1.6-0.65-50 -- bash eval_script_with_cfg_stacked_baseline.sh 1.6 0.65 50
+gantry run $args --workspace ai2/structured_diffusion --cluster ai2/jupiter-cirrascale-2 --cluster ai2/saturn-cirrascale --gpus 8 --name SiTXL2-StackedCFG-Baseline-cfg-1.4-0.65-50 -- bash eval_script_with_cfg_stacked_baseline.sh 1.4 0.65 50
+gantry run $args --workspace ai2/structured_diffusion --cluster ai2/jupiter-cirrascale-2 --cluster ai2/saturn-cirrascale --gpus 8 --name SiTXL2-StackedCFG-Baseline-cfg-1.2-0.65-50 -- bash eval_script_with_cfg_stacked_baseline.sh 1.2 0.65 50
+gantry run $args --workspace ai2/structured_diffusion --cluster ai2/jupiter-cirrascale-2 --cluster ai2/saturn-cirrascale --gpus 8 --name SiTXL2-StackedCFG-Baseline-cfg-1.0-0.65-50 -- bash eval_script_with_cfg_stacked_baseline.sh 1.0 0.65 50
